d198771f15b5e21f6c070cf13931a47d579a4e90
Vinzon <luongvinson@gmail.com>
2022-12-06 11:55:45 -0700

    added constructor to Form component

--- src/App.js
+++ src/App.js
@@ -7,43 +7,7 @@ import Form from "./Components/Form";
 // import logo from './logo.svg';
 
 class App extends Component {
-  constructor() {
-    super();
-
-    this.state = {
-      general: {
-        name: '',
-        email: '',
-        phone: ''
-      },
-      education: {
-        id: uniqid(),
-        school: '',
-        major: '',
-        yearCompleted: ''
-      },
-      experience: {
-        id: uniqid(),
-        company: '',
-        position: '',
-        tasks: '',
-        yearStarted: '',
-        yearCompleted: '',
-      },
-      educationArray: [],
-      experienceArray: []
-    }
-  }
-
-  // onSubmit = (e) => {
-  //   e.preventDefault();
-  //   const value = e.target.value
-  //   this.setState({
-  //     ...this.state,
-  //     [e.target.name]: value
-  //   })
-  // }
-
+  
   render () {
     return (
       <div>
--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,42 +1,53 @@
-import React from "react";
+import {Component} from "react";
+import uniqid from "uniqid";
 
-const Form = () => {
-    return (
-        <div>
-            <form className="form">
-                <section className="general">
-                    <label htmlFor="name">Name</label>
-                    <input type="text" id="name"/>
-                    <label htmlFor="email">Email</label>
-                    <input type="email" id="email"/>
-                    <label htmlFor="phone">Phone</label>
-                    <input type="tel" id="phone"/>
-                </section>
-                <section className="education">
-                    <label htmlFor="school">School</label>
-                    <input type="text" id="school"/>
-                    <label htmlFor="major">Major</label>
-                    <input type="text" id="major"/>
-                    <label htmlFor="yearCompleted">Year Completed</label>
-                    <input type="text" id="yearCompleted"/>
-                </section>
-                <section className="experience">
-                    <label htmlFor="company">Company</label>
-                    <input type="text" id="company"/>
-                    <label htmlFor="position">Position</label>
-                    <input type="text" id="position"/>
-                    <label htmlFor="tasks">Tasks</label>
-                    <input type="text" id="tasks"/>
-                    <label htmlFor="yearStarted">Year Started</label>
-                    <input type="text" id="yearStarted"/>
-                    <label htmlFor="yearEnded">Year Ended</label>
-                    <input type="text" id="yearEnded"/>
-                </section>
-                <button type="submit">Confirm</button>
-            </form>
-        </div>
-    )
-    
+class Form extends Component {
+    constructor(props) {
+        super(props);
+        this.state ={
+            name: '',
+            email: '',
+            phone: ''
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <form className="form">
+                    <section className="general">
+                        <label htmlFor="name">Name</label>
+                        <input type="text" name="name"/>
+                        <label htmlFor="email">Email</label>
+                        <input type="email" name="email"/>
+                        <label htmlFor="phone">Phone</label>
+                        <input type="tel" name="phone"/>
+                    </section>
+                    <section className="education">
+                        <label htmlFor="school">School</label>
+                        <input type="text" name="school"/>
+                        <label htmlFor="major">Major</label>
+                        <input type="text" name="major"/>
+                        <label htmlFor="yearCompleted">Year Completed</label>
+                        <input type="text" name="yearCompleted"/>
+                    </section>
+                    <section className="experience">
+                        <label htmlFor="company">Company</label>
+                        <input type="text" name="company"/>
+                        <label htmlFor="position">Position</label>
+                        <input type="text" name="position"/>
+                        <label htmlFor="tasks">Tasks</label>
+                        <input type="text" name="tasks"/>
+                        <label htmlFor="yearStarted">Year Started</label>
+                        <input type="text" name="yearStarted"/>
+                        <label htmlFor="yearEnded">Year Ended</label>
+                        <input type="text" name="yearEnded"/>
+                    </section>
+                    <button type="submit">Confirm</button>
+                </form>
+            </div>
+        )    
+    }
 }
 
 export default Form;
